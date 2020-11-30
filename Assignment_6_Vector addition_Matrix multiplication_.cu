fe27d342aad7531b50f754147a2982a7c78c2466
Piyush Chaudhari <piyushrc26@gmail.com>
2020-11-30 12:39:59 +0530

    code updated !

--- Assignment_6_Vector addition_Matrix multiplication_.cu
+++ Assignment_6_Vector addition_Matrix multiplication_.cu
@@ -1,19 +1,25 @@
-//%%cu//(only for google colab
-//Author : Piyush Rajendra Chaudhari
-//Roll No: BECOC311
+%%cu
+//Author     : Piyush Rajendra Chaudhari
+//Roll No    : BECOC311
+//Division   : C
+//My GitHub Repository Code Link :
+//https://github.com/piyush26c/Cuda-Programming/blob/main/Assignment_6_Vector%20addition_Matrix%20multiplication_.cu
+//Code Title : Add two vectors parallel and perform n * n matrix multiplication parallelly.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda.h>
-#define VECTOR_SIZE (1 << (VECTOR_SIZE_))   //corresponds to 2 ^VECTOR_SIZE_
-#define VECTOR_SIZE_ 5
 
-__global__ void vectorAddition (long long *vectorA_, long long *vectorB_, long long *vectorC_) {
-	vectorC_[blockIdx.x] = vectorA_[blockIdx.x] + vectorB_[blockIdx.x];
+#define VECTOR_SIZE (1 << (VECTOR_SIZE_))   //corresponds to 2^VECTOR_SIZE_
+#define VECTOR_SIZE_ 3 //means 8 * 8 matrix
+
+__global__ void vectorAddition(long long *vectorA_, long long *vectorB_, long long *vectorC_) {
+    vectorC_[blockIdx.x] = vectorA_[blockIdx.x] + vectorB_[blockIdx.x];
 }
 
-__global__ void vectorMatrixMultiplication (long long *vectorA_, long long *vectorB_, long long *vectorC_) {
-	int row = blockIdx.y*blockDim.y+threadIdx.y;
-    int col = blockIdx.x*blockDim.x+threadIdx.x;
+__global__ void vectorMatrixMultiplication(long long *vectorA_, long long *vectorB_, long long *vectorC_) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
 
     float tmpSum = 0;
 
@@ -26,122 +32,133 @@ __global__ void vectorMatrixMultiplication (long long *vectorA_, long long *vect
     vectorC_[row * VECTOR_SIZE + col] = tmpSum;
 }
 
-void fillVector (long long *vector_) {
-	for (int indx = 0; indx < VECTOR_SIZE; indx++) {
-		vector_[indx] = indx;
-	}
+void fillVector(long long *vector_) {
+    for (int indx = 0; indx < VECTOR_SIZE; indx++) {
+        vector_[indx] = indx;
+    }
 }
 
-void fillMatrixVector (long long *vector_) {
-	for (int indx1 = 0; indx1 < VECTOR_SIZE; indx1++) {
-		for (int indx2 = 0; indx2 < VECTOR_SIZE; indx2++) {
-			vector_[indx1 * VECTOR_SIZE + indx2] = 1;
-		}
-	}
+void fillMatrixVector(long long *vector_) {
+    for (int indx1 = 0; indx1 < VECTOR_SIZE; indx1++) {
+        for (int indx2 = 0; indx2 < VECTOR_SIZE; indx2++) {
+            vector_[indx1 * VECTOR_SIZE + indx2] = 1;
+        }
+    }
 }
 
-void printVector (long long *vector_) {
-	for (int indx = 0; indx < VECTOR_SIZE; indx++) {
-		printf("%lld ", vector_[indx]);
-	}
+void printMatrixVector(long long *vector_) {
+    for (int indx1 = 0; indx1 < VECTOR_SIZE; indx1++) {
+        for (int indx2 = 0; indx2 < VECTOR_SIZE; indx2++) {
+            printf("%lld ", vector_[indx1 * VECTOR_SIZE + indx2]);
+        }
+    }
+}
+
+void printVector(long long *vector_) {
+    for (int indx = 0; indx < VECTOR_SIZE; indx++) {
+        printf("%lld ", vector_[indx]);
+    }
 }
 
 int main(void) {
-	//program for vector addition
-	long long *hostVectorA, *hostVectorB, *hostVectorC;
-	long long *deviceVectorA, *deviceVectorB, *deviceVectorC;
-	long long memorySize = VECTOR_SIZE * sizeof(long long);
-	
-	// Allocate space for host vectors A, B, C and insert input values
-    hostVectorA = (long long *)malloc(memorySize); 
-	fillVector(hostVectorA);
-    hostVectorB = (long long *)malloc(memorySize); 
-	fillVector(hostVectorB);
-    hostVectorC = (long long *)malloc(memorySize);
-	
-	// Allocate space for device vectors A, B, C
-    cudaMalloc((void **)&deviceVectorA, memorySize);
-    cudaMalloc((void **)&deviceVectorB, memorySize);
-    cudaMalloc((void **)&deviceVectorC, memorySize);
-	
-	// Copy vector data from host to device
+    //program for vector addition
+    long long *hostVectorA, *hostVectorB, *hostVectorC;
+    long long *deviceVectorA, *deviceVectorB, *deviceVectorC;
+    long long memorySize = VECTOR_SIZE * sizeof(long long);
+
+    // Allocate space for host vectors A, B, C and insert input values
+    hostVectorA = (long long *) malloc(memorySize);
+    fillVector(hostVectorA);
+    hostVectorB = (long long *) malloc(memorySize);
+    fillVector(hostVectorB);
+    hostVectorC = (long long *) malloc(memorySize);
+
+    // Allocate space for device vectors A, B, C
+    cudaMalloc((void **) &deviceVectorA, memorySize);
+    cudaMalloc((void **) &deviceVectorB, memorySize);
+    cudaMalloc((void **) &deviceVectorC, memorySize);
+
+    // Copy vector data from host to device
     cudaMemcpy(deviceVectorA, hostVectorA, memorySize, cudaMemcpyHostToDevice);
     cudaMemcpy(deviceVectorB, hostVectorB, memorySize, cudaMemcpyHostToDevice);
-	
-	//by creating multiple blocks with single thread in it.
-	dim3 blocksPerGrid(VECTOR_SIZE, 1, 1);
-	dim3 threadsPerBlock(1, 1, 1);
-	vectorAddition<<<blocksPerGrid, threadsPerBlock>>>(deviceVectorA, deviceVectorB, deviceVectorC);
- 
+
+    //by creating multiple blocks with single thread in it.
+    dim3 blocksPerGrid(VECTOR_SIZE, 1, 1);
+    dim3 threadsPerBlock(1, 1, 1);
+    vectorAddition << < blocksPerGrid, threadsPerBlock >> > (deviceVectorA, deviceVectorB, deviceVectorC);
+
     // Copy result back to host
     cudaMemcpy(hostVectorC, deviceVectorC, memorySize, cudaMemcpyDeviceToHost);
- 
-    printf("VECTOR A : ");
-	printVector(hostVectorA);
-	printf("\nVECTOR B : ");
-	printVector(hostVectorB);
-	printf("\nVECTOR C : ");
-	printVector(hostVectorC);
-    free(hostVectorA); 
-	free(hostVectorB); 
-	free(hostVectorC);
- 
+    printf("\nOutput : ");
+    printf("\n\nVector Addition (Parallel Programming) : ");
+    printf("\nVECTOR A : ");
+    printVector(hostVectorA);
+    printf("\nVECTOR B : ");
+    printVector(hostVectorB);
+    printf("\nVECTOR C : ");
+    printVector(hostVectorC);
+    free(hostVectorA);
+    free(hostVectorB);
+    free(hostVectorC);
+
     //free gpu memory
-    cudaFree(deviceVectorA); 
-	cudaFree(deviceVectorB); 
-	cudaFree(deviceVectorC);
-	{//matrix multiplication scopes starts
-	//program for matrix multiplication
-	long long *hostMatrixA, *hostMatrixB, *hostMatrixC;
-	long long *deviceMatrixA, *deviceMatrixB, *deviceMatrixC;
-	//allocate 2-D space
-	long long memoryMatrixSize = VECTOR_SIZE * VECTOR_SIZE * sizeof(long long);
-	
-	// Allocate space for host matrix vectors A, B, C and insert input values
-    hostMatrixA = (long long *)malloc(memoryMatrixSize); 
-	fillMatrixVector(hostMatrixA);
-    hostMatrixB = (long long *)malloc(memoryMatrixSize); 
-	fillMatrixVector(hostMatrixB);
-    hostMatrixC = (long long *)malloc(memoryMatrixSize);
-	
-	// Allocate space for device matrix vectors A, B, C
-    cudaMalloc((void **)&deviceMatrixA, memoryMatrixSize);
-    cudaMalloc((void **)&deviceMatrixB, memoryMatrixSize);
-    cudaMalloc((void **)&deviceMatrixC, memoryMatrixSize);
-	
-	// Copy vector data from host to device
-    cudaMemcpy(deviceMatrixA, hostMatrixA, memoryMatrixSize, cudaMemcpyHostToDevice);
-    cudaMemcpy(deviceMatrixB, hostMatrixB, memoryMatrixSize, cudaMemcpyHostToDevice);
-	
-	//by creating multiple blocks with single thread in it.
-	dim3 blocksPerGrid(1, 1, 1);
-	dim3 threadsPerBlock(VECTOR_SIZE, 1, 1);
-	/*
-	if (VECTOR_SIZE * VECTOR_SIZE > 512){		
-		threadsPerBlock.x = 512;
-        threadsPerBlock.y = 512;
-        blocksPerGrid.x = ceil(double(VECTOR_SIZE)/double(threadsPerBlock.x));
-        blocksPerGrid.y = ceil(double(VECTOR_SIZE)/double(threadsPerBlock.y));
-    }*/
-	vectorMatrixMultiplication<<<blocksPerGrid, threadsPerBlock>>>(deviceMatrixA, deviceMatrixB, deviceMatrixC);
-	
-	// Copy result back to host
-    cudaMemcpy(hostMatrixC, deviceMatrixC, memoryMatrixSize, cudaMemcpyDeviceToHost);
- 
-    printf("\nMATRIX VECTOR A : ");
-	printVector(hostMatrixA);
-	printf("\nMATRIX VECTOR B : ");
-	printVector(hostMatrixB);
-	printf("\nMATRIX VECTOR C : ");
-	printVector(hostMatrixC);
-    free(hostMatrixA); 
-	free(hostMatrixB); 
-	free(hostMatrixC);
- 
-    //free gpu memory
-    cudaFree(deviceMatrixA); 
-	cudaFree(deviceMatrixB); 
-	cudaFree(deviceMatrixC);
-	}//matrix multiplication scopes ends
-	return 0;
+    cudaFree(deviceVectorA);
+    cudaFree(deviceVectorB);
+    cudaFree(deviceVectorC);
+    {//matrix multiplication scope starts
+        //program for matrix multiplication
+        long long *hostMatrixA, *hostMatrixB, *hostMatrixC;
+        long long *deviceMatrixA, *deviceMatrixB, *deviceMatrixC;
+        //allocate 2-D space
+        long long memoryMatrixSize = VECTOR_SIZE * VECTOR_SIZE * sizeof(long long);
+
+        // Allocate space for host matrix vectors A, B, C and insert input values
+        hostMatrixA = (long long *) malloc(memoryMatrixSize);
+        fillMatrixVector(hostMatrixA);
+        hostMatrixB = (long long *) malloc(memoryMatrixSize);
+        fillMatrixVector(hostMatrixB);
+        hostMatrixC = (long long *) malloc(memoryMatrixSize);
+
+        // Allocate space for device matrix vectors A, B, C
+        cudaMalloc((void **) &deviceMatrixA, memoryMatrixSize);
+        cudaMalloc((void **) &deviceMatrixB, memoryMatrixSize);
+        cudaMalloc((void **) &deviceMatrixC, memoryMatrixSize);
+
+        // Copy vector data from host to device
+        cudaMemcpy(deviceMatrixA, hostMatrixA, memoryMatrixSize, cudaMemcpyHostToDevice);
+        cudaMemcpy(deviceMatrixB, hostMatrixB, memoryMatrixSize, cudaMemcpyHostToDevice);
+
+        //by creating multiple blocks with single thread in it.
+        dim3 blocksPerGrid(1, 1, 1);
+        dim3 threadsPerBlock(VECTOR_SIZE, VECTOR_SIZE, 1);
+
+        if (VECTOR_SIZE * VECTOR_SIZE > 512) {
+            threadsPerBlock.x = 512;
+            threadsPerBlock.y = 512;
+            blocksPerGrid.x = ceil(double(VECTOR_SIZE) / double(threadsPerBlock.x));
+            blocksPerGrid.y = ceil(double(VECTOR_SIZE) / double(threadsPerBlock.y));
+        }
+
+        vectorMatrixMultiplication <<< blocksPerGrid, threadsPerBlock >>> (deviceMatrixA, deviceMatrixB, deviceMatrixC);
+
+        // Copy result back to host
+        cudaMemcpy(hostMatrixC, deviceMatrixC, memoryMatrixSize, cudaMemcpyDeviceToHost);
+        printf("\n\nMatrix Multiplication (Parallel Programming) : ");
+        printf("\nMatrix Size : %d * %d", VECTOR_SIZE, VECTOR_SIZE);
+        printf("\nMATRIX VECTOR A : ");
+        printMatrixVector(hostMatrixA);
+        printf("\nMATRIX VECTOR B : ");
+        printMatrixVector(hostMatrixB);
+        printf("\nMATRIX VECTOR C : ");
+        printMatrixVector(hostMatrixC);
+        free(hostMatrixA);
+        free(hostMatrixB);
+        free(hostMatrixC);
+
+        //free gpu memory
+        cudaFree(deviceMatrixA);
+        cudaFree(deviceMatrixB);
+        cudaFree(deviceMatrixC);
+    }//matrix multiplication scope ends
+    return 0;
 }
